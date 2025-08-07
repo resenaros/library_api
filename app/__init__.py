83559a0ab564fd850ddeaeae211f40e0fd388ba1
Ariel R <resendiz.ariel6@gmail.com>
2025-08-06 22:17:51 -0700

    added preliminary Yaml documentation

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,20 @@ from .blueprints.books import books_bp
 from .blueprints.items import items_bp
 from .blueprints.orders import orders_bp
 from flask_migrate import Migrate
+from flask_swagger_ui import get_swaggerui_blueprint
 
 migrate = Migrate()
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/swagger.yaml'  # Our API URL (can of course be a local resource)
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Your API's Name"
+    }
+)
 
 def create_app(config_name=None):
     app = Flask(__name__)
@@ -32,6 +44,7 @@ def create_app(config_name=None):
     app.register_blueprint(books_bp, url_prefix='/books')
     app.register_blueprint(items_bp, url_prefix='/items')
     app.register_blueprint(orders_bp, url_prefix='/orders')
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL) # Register Swagger UI blueprint
 
     return app
     
